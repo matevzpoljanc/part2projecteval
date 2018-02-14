b141279876bd73b68d65da0e0679e6c3384055c9
Matevz Poljanc <matevz.poljanc@gmail.com>
2018-02-14 23:58:00 +0000

    Fix Travis-CI

--- evaluate.ml
+++ evaluate.ml
@@ -3,7 +3,7 @@ open Core_kernel
 
 open Travelling_salesman
 let benchmark = Bench.Test.create_indexed ~name:"Travelling salesman" 
-                        (fun x -> Staged.stage @@ fun () -> ignore @@ travelling_salesman @@ generate_graph ~range:10 (x+2))
+                        (fun x -> Staged.stage @@ fun () -> ignore @@ travelling_salesman @@ generate_graph ~range:10 (x))
 
 let () =
      Bench.bench [benchmark ~args:[2;4;6;8]]
